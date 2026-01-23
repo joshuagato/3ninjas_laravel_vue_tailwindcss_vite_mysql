ecd7bd4d5e296face8db193b7083517019aceb82
Joshua Gato <joshuagato37@gmail.com>
2026-01-23 20:09:02 +0000

    Updated the app url

--- resources/js/axiosConfigWeb.js
+++ resources/js/axiosConfigWeb.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const baseURL = process.env.VUE_APP_API_BASE_URL || 'http://127.0.0.1:8000';
 // const baseURL = process.env.VITE_APP_API_BASE_URL || 'http://127.0.0.1:8000';
-const baseURL = "http://127.0.0.1:8000";
+const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
 
 const axiosInstance = axios.create({
     baseURL,
